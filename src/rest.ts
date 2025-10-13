9c2943e7d77d9ba03a543203b8239f5aca835f4b
Lucien Aibel <lucien.aibel@mymail.champlain.edu>
2025-10-13 20:24:35 +0100

    Floating-point processing now happening on backend, and stores can now return lat, long, and multiple types

--- src/rest.ts
+++ src/rest.ts
@@ -74,14 +74,13 @@ async function handleItemGet(c: Context<{ Bindings: Env }>): Promise<Response> {
 	const types = c.req.queries("type");
 	
 	try {
-		let query = `SELECT * FROM Items JOIN Categories ON Items.categoryID = Categories.categoryID JOIN Stores ON Items.storeID = Stores.storeID`;
+		let query = `SELECT Items.itemID, itemName, price, Items.storeID, Items.categoryID FROM Items JOIN Categories ON Items.categoryID = Categories.categoryID JOIN Stores ON Items.storeID = Stores.storeID`;
 		if (types != undefined) {
 			query += " JOIN StoreTypes ON Stores.storeID = StoreTypes.storeID JOIN Types ON StoreTypes.typeID = Types.typeID"
 		}
 		
 		//For each parameter, add it to the query string and list of parameterized inputs (the values array)
 		//Parameterized inputs are used because they prevent SQL injections, according to the internet
-		
 		let built = buildQueryParams(query, items, categories, stores, types)
 		query = built[0]
 		let values = built[1]
@@ -91,7 +90,14 @@ async function handleItemGet(c: Context<{ Bindings: Env }>): Promise<Response> {
 			.bind(...values)
 			.all();
 
-		return c.json(results);
+		//Processing results
+		let ret = {}
+		for (let item of results.results) {
+			//Build an object for each item, allowing us to do the division of the stored integer price for them
+			ret[item.itemID] = {"itemID":item.itemID, "itemName":item.itemName,"price":(item.price / 100), "storeID":item.storeID, "categoryID":item.categoryID}
+		}
+
+		return c.json(ret);
 	} catch (error: any) {
 		return c.json({ error: error.message }, 500);
 	}
@@ -124,8 +130,12 @@ async function handleCategoriesGet(c: Context<{ Bindings: Env }>): Promise<Respo
 		const results = await c.env.DB.prepare(query)
 			.bind(...values)
 			.all();
+		
+		//Processing results
+		//(right now, just to match formatting with the results that do need processing)
+		let ret = results.results
 
-		return c.json(results);
+		return c.json(ret);
 	} catch (error: any) {
 		return c.json({ error: error.message }, 500);
 	}
@@ -138,7 +148,7 @@ async function handleStoresGet(c: Context<{ Bindings: Env }>): Promise<Response>
 	const types = c.req.queries("type");
 	
 	try {
-		let query = `SELECT Stores.storeID, storeName, description, website, address, typeName FROM Stores JOIN StoreTypes ON Stores.storeID = StoreTypes.storeID JOIN Types ON StoreTypes.typeID = Types.typeID`;
+		let query = `SELECT Stores.storeID, storeName, description, website, address, StoreTypes.typeID, typeName, latitude, longitude FROM Stores JOIN StoreTypes ON Stores.storeID = StoreTypes.storeID JOIN Types ON StoreTypes.typeID = Types.typeID`;
 		if (items != undefined || categories != undefined) {
 			query += " JOIN Items ON Items.storeID = Stores.storeID"
 		}
@@ -150,15 +160,33 @@ async function handleStoresGet(c: Context<{ Bindings: Env }>): Promise<Response>
 		query = built[0]
 		let values = built[1]
 		
-		//GROUP BY has to go last, and will make sure that no matter how many items are in a category, we just return one listing for it
-		query += " GROUP BY Stores.storeID"
-		
 		//Send the query and return the results
 		const results = await c.env.DB.prepare(query)
 			.bind(...values)
 			.all();
 		
-		return c.json(results);
+		//Processing results
+		let ret = {}
+		for (let store of results.results) {
+			//Prebuild an object of all the type data from the entry
+			//makes later stuff faster
+			//(remember that if a store has multiple types, they will show up on two different rows with otherwise duplicate information)
+			//(this is the whole reason we have this processing, pretty much)
+			let typeObject = {"typeID":store.typeID, "typeName":store.typeName}
+			
+			//If this store is not yet in the output, build an object and add
+			if (!(store.storeID in ret)) {
+				ret[store.storeID] = {"storeID":store.storeID, "storeName":store.storeName,"description":store.description,"website":store.website,"address":store.address,"type":[typeObject],"latitude":(store.latitude / 1000000), "longitude":(store.longitude / 1000000)}
+				
+			//If this is an entry for a store already in the output, then it's a listing for a different type associated with that store. Add that to the existing entry.
+			} else {
+				if (!(typeObject in ret[store.storeID]["type"])) {
+					ret[store.storeID]["type"].push(typeObject)
+				}
+			}
+		}
+		
+		return c.json(ret);
 	} catch (error: any) {
 		return c.json({ error: error.message }, 500);
 	}
@@ -192,7 +220,7 @@ async function handlePostItem(c: Context<{ Bindings: Env }>): Promise<Response> 
 		const query = "INSERT INTO Items(itemName, price, storeID, categoryID) VALUES (?,?,?,?)"
 		
 		const result = await c.env.DB.prepare(query)
-			.bind(data.itemName, data.price, data.storeID, data.categoryID)
+			.bind(data.itemName, data.price / 100, data.storeID, data.categoryID)
 			.run();
 			
 		return c.json({ message: 'Item added', data }, 201);
